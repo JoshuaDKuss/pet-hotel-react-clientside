6dfb88b2cbb89461510ceef066bf026d38f332df
bairdh <hrbaird10@gmail.com>
2020-06-11 16:20:36 -0500

    merging

--- src/Components/AddPet/AddPet.jsx
+++ src/Components/AddPet/AddPet.jsx
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export class AddPet extends Component {
+
+    state = {
+        name: '',
+        color: '',
+        breed: '',
+        owner: '',
+        checkIn: false
+    }
 
 componentDidMount = () => {
     console.log('comp did mount AddPet');
     //this.props.dispatch({ type: 'GET_PETS' });
 }
 
+handleChange = (event, prop) => {
+    this.setState({
+        [prop]: event.target.value
+    })
+}
+
+addPet = () => {
+    this.props.dispatch({type:"POST_PET", payload: this.state});
+
+}
 
     render() {
+        console.log(this.state);
+        
         return (
             <div>
 
@@ -16,14 +37,14 @@ componentDidMount = () => {
                 <div className="addForm">
                 <h2>Add Pet</h2>
                 <form>
-                <input placeholder="Pet Name"></input>
-                <input placeholder="Pet Color"></input>
-                <input placeholder="Pet Breed"></input>
-                <select>
-                    <option>"Owner Name"</option>
+                <input onChange={event => this.handleChange(event, "name")} placeholder="Pet Name"></input>
+                <input onChange={event => this.handleChange(event, "color")} placeholder="Pet Color"></input>
+                <input onChange={event => this.handleChange(event, "breed")} placeholder="Pet Breed"></input>
+                        <select onChange={event => this.handleChange(event, "owner")}>
+                    <option value="default">"Owner Name"</option>
                 </select>
                 <button>Check In</button>
-                <button>Submit</button>
+                <button onClick={this.addPet}>Submit</button>
                 </form>
                 </div>
 
@@ -33,4 +54,6 @@ componentDidMount = () => {
     }
 }
 
-export default AddPet;
+const propsToRedux = redux => ({redux});
+
+export default connect(propsToRedux)(AddPet);
