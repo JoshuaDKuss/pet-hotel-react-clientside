35371a5041f7374838a6e718a441209044466a73
Josh Kuss <joshua.d.kuss@gmail.com>
2020-06-11 10:55:32 -0500

    building App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {
+  HashRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+import {connect} from 'react-redux';
+
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Pet Hotel</h1>
+        </header>
+        <Switch>
+          <Redirect exact from="/" to="/pet" />
+          <Route
+              exact
+              path="/pet"
+              component={AddPet}
+            />
+            <Route
+              exact
+              path="/owner"
+              component={AddOwner}
+            />
+        </Switch>
+      </div>
+    </Router>
+    );
+  }
 }
 
-export default App;
+export default connect()(App);
