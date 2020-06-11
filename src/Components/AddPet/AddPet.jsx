93a6734e4277aa49faefd298a62b15f14b9c5e4e
Josh Kuss <joshua.d.kuss@gmail.com>
2020-06-11 11:47:12 -0500

    buiding index.js

--- src/Components/AddPet/AddPet.jsx
+++ src/Components/AddPet/AddPet.jsx
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 
 export class AddPet extends Component {
+
+componentDidMount = () => {
+    console.log('comp did mount AddPet');
+    //this.props.dispatch({ type: 'GET_PETS' });
+}
+
+
     render() {
         return (
             <div>
+
+
+                <div className="addForm">
                 <h2>Add Pet</h2>
+                <form>
                 <input placeholder="Pet Name"></input>
                 <input placeholder="Pet Color"></input>
                 <input placeholder="Pet Breed"></input>
                 <select placeholder="Owner Name"></select>
                 <button>Submit</button>
+                </form>
+                </div>
+
+
             </div>
         )
     }
