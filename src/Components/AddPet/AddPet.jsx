9efa83eda581ed7a4ca48cf6d897a9d60207e9d4
Josh Kuss <joshua.d.kuss@gmail.com>
2020-06-11 14:33:04 -0500

    many changes made to AddPet.jsx, added AddPet.css

--- src/Components/AddPet/AddPet.jsx
+++ src/Components/AddPet/AddPet.jsx
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 //import { connect } from 'react-redux';
-
+import './AddPet.css';
 
 export class AddPet extends Component {
 
+    state = {
+        newPet: {
+            pet_name: '',
+            pet_color: '',
+            pet_breed: '',
+            owner: '',
+        }
+    }
 
-componentDidMount = () => {
-    console.log('comp did mount AddPet');
-    //this.props.dispatch({ type: 'GET_PETS' });
-}
+    componentDidMount = () => {
+        console.log('comp did mount AddPet');
+        //this.props.dispatch({ type: 'GET_PETS' });
+    }
 
-toHome = () => {
-    //this.props.history.push(`/`);
-}
+    toHome = () => {
+        //this.props.history.push(`/`);
+    }
+
+    handleChangeFor = (event) => {
+        // event.target.name is coming from name attribute on input field
+        let propName = event.target.name;
+        console.log('Property:', propName);
+        this.setState({
+            newPet: {
+                ...this.state.newPet,
+                // event.target.value has the value (typed in) from the input field
+                [propName]: event.target.value,
+            }
+        })
+    }
 
 
     render() {
@@ -21,21 +42,58 @@ toHome = () => {
 
 
                 <div className="addPetForm">
-                <h2>Add Pet</h2>
-                <form>
-                <input placeholder="Pet Name"></input>
-                <input placeholder="Pet Color"></input>
-                <input placeholder="Pet Breed"></input>
-                <select>
-                    <option>Owner Name</option>
-                    <option></option>
-                    <option></option>
-                </select>
-                <button>Check In</button>
-                <button>ADD</button>
-                </form>
-                </div>
+                    <h2>Add Pet</h2>
+                    <form>
+                        <input placeholder="Pet Name" name="pet_name"
+                        onChange={this.handleChangeFor}
+                        value={this.state.newPet.pet_name}></input><br/>
 
+                        <input placeholder="Pet Color" name="pet_color"
+                        onChange={this.handleChangeFor}
+                        value={this.state.newPet.pet_color}></input><br/>
+
+                        <input placeholder="Pet Breed" name="pet_breed"
+                        onChange={this.handleChangeFor}
+                        value={this.state.newPet.pet_breed}></input><br/>
+
+                        <select name="owner_id"
+                        value={this.state.newPet.owner}
+                        onChange={this.handleChangeFor}>
+                            <option>1 Owner Name</option>
+                            <option>2 Owner Name</option>
+                            <option>3 Owner Name</option>
+                        </select>
+
+                        <br/><button type="submit">ADD PET</button>
+                    </form>
+                </div><br/><br/>
+
+
+                <h3>History</h3><br/>
+                <table>
+                    <thead>
+                    <tr>
+                    <th className='tHead'>Owner Name</th>
+                    <th className='tHead'>Pet Name</th>
+                    <th className='tHead'>Breed</th>
+                    <th className='tHead'>Color</th>
+                    <th className='tHead'>Status</th>
+                    <th className='tHead'>Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                    {/* {this.props.reduxState.pets.map(pet => <tr key={pet.id} pet={pet}> */}
+                    {/* <td className='tData'>{pet.owner}</td>
+                    <td className='tData'>{pet.pet_name}</td>
+                    <td className='tData'>{pet.pet_breed}</td>
+                    <td className='tData'>{pet.pet_color}</td>
+                    {pet.checked_in ? <td className='tData'>In</td> : <td className='tData'>Out</td> }
+                    <td className='tData'>{}</td> */}
+                  {/* /</tr> */}
+                  {/* ) */}
+                  {/* } */}
+                </tbody>
+            </table> 
 
             </div>
         )
@@ -43,3 +101,7 @@ toHome = () => {
 }
 
 export default AddPet;
+
+// const mapReduxStateToProps = (reduxState) => ({reduxState })
+
+// export default connect(mapReduxStateToProps)(AddPet);
