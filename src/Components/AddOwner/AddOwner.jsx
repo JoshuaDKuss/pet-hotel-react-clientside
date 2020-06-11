24dec00feb215984ded436d0787f98444504286b
ThyChristiansen <thyvu0411@gmail.com>
2020-06-11 16:02:26 -0500

    set up client-side

--- src/Components/AddOwner/AddOwner.jsx
+++ src/Components/AddOwner/AddOwner.jsx
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
 
 export class AddOwner extends Component {
+    state = {
+        ownerName : ''
+    }
+    handleChangeFor = (event) => {
+        console.log('changing',event.target.value)
+        this.setState({
+            ownerName: event.target.value,
+        })
+    }
+
+    handleAddOwner = () =>{
+        console.log('clicked!',this.state.ownerName)
+        this.props.dispatch({
+            type: 'ADD_OWNER',
+            payload: {
+                ownerName: this.state.ownerName
+            }
+        })
+    }
+
+
     render() {
+        
+       
         return (
             <div>
                 <h2>Add Owner</h2>
-                <input placeholder="Owner name"></input>
-                <button>Submit</button>
+                <input placeholder="Owner name" onChange = {this.handleChangeFor}></input>
+                <button onClick = {this.handleAddOwner}>Submit</button>
             </div>
         )
     }
 }
 
-export default AddOwner;
+export default connect()(AddOwner);
+
