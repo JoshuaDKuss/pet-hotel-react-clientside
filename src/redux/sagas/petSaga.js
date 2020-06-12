869ae7d1d3c2e3700217cea2641727dea53f866b
Alan Henderson <mr.alan.henderson@gmail.com>
2020-06-12 09:44:06 -0500

    reducers connected and syntax corectd on Set_pets

--- src/redux/sagas/petSaga.js
+++ src/redux/sagas/petSaga.js
@@ -8,8 +8,8 @@ function* petSaga() {
 function* fetchPets(){
     console.log('in fetchPets');
     try{
-        const response = yield axios.get('/api/user');
-        yield put({ type: 'SET_USER', payload: response.data});
+        const response = yield axios.get('/api/pet');
+        yield put({ type: 'SET_PETS', payload: response.data});
     }catch (error) {
         console.log("Pet GET request failed", error);
         
