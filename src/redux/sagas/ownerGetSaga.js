75ef2dc4f17af5c3c46c177f4f183f0fdaf5ccbc
Josh Kuss <joshua.d.kuss@gmail.com>
2020-06-12 11:22:44 -0500

    slight edit to ownerGetSaga

--- src/redux/sagas/ownerGetSaga.js
+++ src/redux/sagas/ownerGetSaga.js
@@ -4,7 +4,7 @@ import { put, takeLatest } from 'redux-saga/effects';
 function* fetchOwner(){
     console.log('fetchOwner');
     try{
-        const response = yield axios.get('/owner');
+        const response = yield axios.get('/api/owner');
         yield put({ type: 'SET_OWNER', payload: response.data });
     } catch (error) {
         console.log('User get request failed', error);
